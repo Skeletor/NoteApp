55e4e5268002eeb88e7692354f17e7b365bbdd20
Ruslan_Belov <belovruslan15@mail.ru>
2021-09-20 21:43:24 +0700

    Revert "Revert "(TEST) Добавлен отступ 2""
    This reverts commit e023d8477c6231d339b01c38fe138eae4f66922e.

--- NoteApp/NoteAppUI/Program.cs
+++ NoteApp/NoteAppUI/Program.cs
@@ -15,6 +15,7 @@ namespace NoteAppUI
         static void Main()
         {
 
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
