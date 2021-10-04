b89df1894887291fe0a176d463df9c03d86fd318
Ruslan_Belov <belovruslan15@mail.ru>
2021-10-04 18:58:48 +0700

    реализован интерфейс icloneable

--- NoteApp/NoteAppUI/Form1.cs
+++ NoteApp/NoteAppUI/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NoteApp;
 
 namespace NoteAppUI
 {
@@ -16,5 +17,7 @@ namespace NoteAppUI
         {
             InitializeComponent();
         }
+
+        
     }
 }
