9430e8454ec5ece56f6dbadc3f9b5b7d813f3c53
Ruslan_Belov <belovruslan15@mail.ru>
2021-10-14 17:09:57 +0700

    добавлены классы "проект". "менеджер проекта". написана логика сериализации/десериализации

---
+++ NoteApp/NoteApp/ProjectManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace NoteApp
+{
+    public class ProjectManager
+    {
+        private readonly string configPath = $"{Environment.CurrentDirectory}\\NoteApp\\dataConfig.json";
+
+        public void SaveTo(Project project)
+        {
+            using (StreamWriter sw = new StreamWriter(configPath))
+            using (JsonWriter jr = new JsonTextWriter(sw))
+            {
+                new JsonSerializer().Serialize(jr, project);
+            }
+        }
+
+        public void LoadFrom()
+        {
+            using (StreamReader sr = new StreamReader(configPath))
+            using (JsonReader jr = new JsonTextReader(sr))
+            {
+                Note note = (Note)new JsonSerializer().Deserialize<Note>(jr);
+            }
+        }
+    }
+}
