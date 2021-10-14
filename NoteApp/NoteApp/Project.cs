c92b2577cf2502e5efb553c12766242e5ece8fd0
Ruslan_Belov <belovruslan15@mail.ru>
2021-10-14 23:06:59 +0700

    Разработана логика приложения: Note.cs, NoteCategory.cs, Project.cs, ProjectManager.cs

--- NoteApp/NoteApp/Project.cs
+++ NoteApp/NoteApp/Project.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace NoteApp
 {
+    /// <summary>
+    /// Класс "Проект", хранящий список всех заметок
+    /// </summary>
     public class Project
     {
         private List<Note> notes;
